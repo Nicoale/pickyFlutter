245d5407fd7a69597b63e5b1c0b71bcbeb295263
nicoale <nicoale8786@gmail.com>
2020-06-25 11:12:36 +0300

    added the textfields for signup

--- lib/Screens/SignUpScreen.dart
+++ lib/Screens/SignUpScreen.dart
@@ -20,6 +20,38 @@ class _SignUpScreenState extends State<SignUpScreen> {
           ),
           SizedBox(
             height:20,
+          ),
+          Padding ( 
+            padding : const EdgeInsets.all(20),
+            child: Row(
+              children: <Widget>[  
+                IconButton(icon: Icon(Icons.person), onPressed: (){}),
+                Expanded(child:
+                Container(  
+                  margin: EdgeInsets.only(left: 4, right: 20),
+                  child: TextField(  
+                    decoration: InputDecoration(   
+                      hintText: 'Username'
+                    ),
+                  )))
+            ],
+            )
+          ),
+                   Padding ( 
+            padding : const EdgeInsets.all(20),
+            child: Row(
+              children: <Widget>[  
+                IconButton(icon: Icon(Icons.lock), onPressed: (){}),
+                Expanded(child:
+                Container(  
+                  margin: EdgeInsets.only(left: 4, right: 20),
+                  child: TextField(  
+                    decoration: InputDecoration(   
+                      hintText: 'Password'
+                    ),
+                  )))
+            ],
+            )
           )
         ]
       ),
